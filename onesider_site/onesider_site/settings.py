a9b79d36344a05efeeafa5ec9b8cf5ec79728c44
EGL Commander <jaiminmehta441@gmail.com>
2026-01-07 23:38:24 +0530

    Models with MEDIA.
    Successfully Integrated and Tested models Category, Tag, and Project. Also Configured how Media Works.

--- onesider_site/onesider_site/settings.py
+++ onesider_site/onesider_site/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for onesider_site project.
 
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'projects'
 ]
 
 MIDDLEWARE = [
@@ -118,6 +120,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [ BASE_DIR / 'static']
+
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ onesider_site/projects/models.py
@@ -0,0 +1,99 @@
+from django.db import models
+from django.utils.text import slugify
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=120, unique=True)
+    description = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=60, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(
+        max_length=220, unique=True, blank=True, editable=False
+        )
+
+    short_description = models.CharField(max_length=300)
+    description = models.TextField()
+
+    categories = models.ManyToManyField(Category, related_name='projects')
+    tags = models.ManyToManyField(Tag, related_name='projects', blank=True)
+
+    PROJECT_TYPE_CHOICES = [
+        ('web', 'Web Application'),
+        ('mobile', 'Mobile Application'),
+        ('game', 'Game'),
+        ('tool', 'Tool / Utility'),
+        ('other', 'Other'),
+    ]
+
+    project_type = models.CharField(
+        max_length=20,
+        choices=PROJECT_TYPE_CHOICES,
+    )
+
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    views_count = models.PositiveIntegerField(default=0)
+    purchases_count = models.PositiveIntegerField(default=0)
+    saves_count = models.PositiveIntegerField(default=0)
+
+    CLOUD_STORAGE_CHOICES = [
+        ('gdrive', 'Google Drive'),
+        ('dropbox', 'Dropbox'),
+        ('onedrive', 'OneDrive'),
+        ('other', 'Other'),
+    ]
+
+    cloud_storage_type = models.CharField(
+        max_length=20,
+        choices=CLOUD_STORAGE_CHOICES
+    )
+
+
+    cloud_file_id = models.CharField(
+        max_length=255,
+        help_text="Internal cloud file/folder ID (NOT a public link)"
+        )
+
+    is_active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            base_slug = slugify(self.title)
+            slug =base_slug
+            counter = 1
+
+            while Project.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+
+            self.slug = slug
+
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
