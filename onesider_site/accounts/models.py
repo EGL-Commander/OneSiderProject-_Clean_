c1fe39a6af82e29fbc67605f7bdc7f1b2d84265b
EGL Commander <jaiminmehta441@gmail.com>
2026-01-11 23:11:41 +0530

    Update Profile
    Added Functionality for the User to Edit Their Display Name and Avatar, After Adding the 'display_name' Field in the Profile Model.

--- onesider_site/accounts/models.py
+++ onesider_site/accounts/models.py
@@ -4,11 +4,21 @@ from projects.models import Project
 
 # Create your models here.
 
+class AvatarChoices(models.IntegerChoices):
+    AVATAR_1 = 1, 'Avatar One'
+    AVATAR_2 = 2, 'Avatar Two'
+    AVATAR_3 = 3, 'Avatar Three'
+    AVATAR_4 = 4, 'Avatar Four'
+    AVATAR_5 = 5, 'Avatar Five'
+    AVATAR_6 = 6, 'Avatar Six'
+    AVATAR_7 = 7, 'Avatar Seven'
+
 class Profile(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE
     )
+    display_name = models.CharField(max_length=50)
 
     purchased_projects = models.ManyToManyField(
         Project,
@@ -20,6 +30,11 @@ class Profile(models.Model):
         Project,
         related_name='saved_by',
         blank=True
+    )
+
+    avatar = models.PositiveSmallIntegerField(
+        choices = AvatarChoices.choices,
+        default = AvatarChoices.AVATAR_1
     )
 
     created_at = models.DateTimeField(auto_now_add=True)
--- onesider_site/accounts/views.py
+++ onesider_site/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import ProfileUpdateForm
 
 # Create your views here.
+
+@login_required
+def edit_profile(request):
+    profile = request.user.profile
+    
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile_edit')
+    else:
+        form = ProfileUpdateForm(instance=profile)
+
+    return render(request, 'accounts/edit_profile.html', {'form' : form})
