c2ec6f2248834c54ba297a289d927b518949306f
EGL Commander <jaiminmehta441@gmail.com>
2026-01-17 10:58:31 +0530

    Projects Catalog Page
    Successfully Implemented a 'Projects Catalog Page'.

--- onesider_site/onesider_site/urls.py
+++ onesider_site/onesider_site/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('accounts/', include('accounts.urls')),
+    path('projects/', include('projects.urls')),
 ]
 
 if settings.DEBUG:
--- onesider_site/projects/views.py
+++ onesider_site/projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+
+def project_list(request):
+    projects = Project.objects.filter(is_active=True).order_by('?')
+    return render(request, 'projects/project_list.html', {'projects': projects})
