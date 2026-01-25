ff1a0db086b157ab63bda55953e3c528cfb581e3
EGL Commander <jaiminmehta441@gmail.com>
2026-01-25 23:53:51 +0530

    Login / Logout Redirecting.
    Successfully Implemented and Tested Logged Out Behavior and Login with Redirect.

--- onesider_site/onesider_site/settings.py
+++ onesider_site/onesider_site/settings.py
@@ -131,3 +131,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login URLs
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
