973c98c71ea906b603f35f7562df12cf3849de61
EGL Commander <jaiminmehta441@gmail.com>
2026-03-17 22:27:18 +0530

    Nav Bar & Home Page
    Added Nav Bar with Home Page Set Up at the Root URL.

--- onesider_site/main/views.py
+++ onesider_site/main/views.py
@@ -8,9 +8,12 @@ def home(request):
 	categories = Category.objects.order_by('name')
 	random_projects = Project.objects.filter(is_active=True).order_by('?')[:4]
 
+	latest_projects = Project.objects.filter(is_active=True).order_by('-created_at')[:6]
+
 	context = {
 		'categories': categories,
 		'random_projects': random_projects,
+		'latest_projects': latest_projects,
 	}
 
 	return render(request, 'main/home.html', context)
