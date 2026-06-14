66b36fc2caa80b6ebd364f61b12385e1b8700b98
EGL Commander <jaiminmehta441@gmail.com>
2026-06-14 11:52:40 +0530

    Logout Issue Fixed
    Fixed the Logout Issue Occurring When Pressing the Option. Newer Django Version Requires POST Method Instead of GET.

--- onesider_site/accounts/urls.py
+++ onesider_site/accounts/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/Login (Latest).html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page = reverse_lazy('project_list')), name='logout'),
     path('register/', views.register_view, name='register'),
     path('profile/', views.profile_view, name='profile'),
     path('profile/edit/', views.edit_profile, name='profile_edit'),
